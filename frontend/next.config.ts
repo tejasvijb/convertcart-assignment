fbf221d0af0081b54eb8ee4452cb9197635283a8
tejasvijb <jbtejasvi@gmail.com>
2025-10-08 11:59:37 +0530

    update next.config.ts to include environment variables for product and segment services

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  /* config options here */
+    output: "standalone",
+    /* config options here */
+    env: {
+        NEXT_PUBLIC_PRODUCT_SERVICE_URL:
+            process.env.NEXT_PUBLIC_PRODUCT_SERVICE_URL,
+        NEXT_PUBLIC_SEGMENT_SERVICE_URL:
+            process.env.NEXT_PUBLIC_SEGMENT_SERVICE_URL,
+    },
 };
 
 export default nextConfig;
